990c6d6c330559ec3fcc9ab4a79a8afb4c9d8424
tklein1801 <38816229+tklein1801@users.noreply.github.com>
2020-05-07 01:51:21 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <title>DulliAG Wartungsarbeiten</title>
+    <meta name="theme-color" content="#303030">
+    <meta property="og:image" content="assets/img/DulliAG%20Logo.jpg">
+    <meta name="description" content="Die Webseite der DulliAG befindet sich derzeit im Wartung. Bitte schauen Sie doch später vorbei.">
+    <meta property="og:type" content="website">
+    <link rel="icon" type="image/jpeg" sizes="500x500" href="assets/img/DulliAG%20Logo.jpg">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css?h=da69eac6171f4aaf29b6035caf94fca0">
+</head>
+
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-expand">
+            <a class="navbar-brand" href="#">DulliAG</a>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <!-- <a href="./Login/" class="btn btn-sm btn-success rounded-pill">Anmelden</a> -->
+                    </li>
+                </ul>
+            </div><!-- ./navbar-collapse -->
+        </nav><!-- ./navbar -->
+
+        <div id="content">
+            <h1 class="title text-center">
+                <span>⚠️</span>
+                Wartungsarbeiten
+                <span>⚠️</span>
+            </h1>
+            <p class="text text-center">
+                Schau doch gerne mal später vorbei👷
+            </p>
+        </div>
+
+
+
+    </div><!-- ./wrapper -->
+
+    <!-- Required Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
